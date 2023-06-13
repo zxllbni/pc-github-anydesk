c618591a09cfa993d4a83f1e5a272a5f0f522ebd
Jahid Hasan <130596733+jahidrdpschool@users.noreply.github.com>
2023-06-13 20:29:09 +0600

    Update DL.bat

--- DL.bat
+++ DL.bat
@@ -3,7 +3,7 @@ powershell -Command "choco install anydesk -y"
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/jahidrdpschool/pc-github-anydesk/main/ST.bat -OutFile ST.bat"
 
 curl -s -L -o C:\Users\Public\Desktop\YTC.zip https://github.com/jahidrdpschool/pc-github-anydesk/raw/main/YTC.zip
-curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
+curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://github.com/jahidrdpschool/pc-github-anydesk/raw/main/Winrar.exe
 powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
 C:\Users\Public\Desktop\Telegram.exe /VERYSILENT /NORESTART
 del C:\Users\Public\Desktop\Telegram.exe
